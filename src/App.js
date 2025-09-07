444574527c56d9e91bb7752ae18b2eab9ebe522a
Alex <alexgonzalvez2005@gmail.com>
2025-09-07 13:06:58 +0200

    Nueva Sesion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import background from "./images/debian_background_01.png";
+import mouse from "./images/mouse.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      style={{ 
+      backgroundImage: `url(${background})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      overflow: "hidden",
+      width: "100vw",
+      height: "100vh",
+      margin: 0,
+      cursor: `url(${mouse}), auto`,
+    }}
+    >
+
     </div>
   );
+
 }
 
 export default App;
